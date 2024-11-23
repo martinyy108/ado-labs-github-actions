2915d72aaa701c335376be1288fa1531e099b53f
martin.yang <abc108cn@hotmail.com>
2024-11-23 11:22:11 +1100

    Refactor Terraform configuration: update variable defaults, add outputs, and include tags for resource management

--- main/variables.tf
+++ main/variables.tf
@@ -4,28 +4,28 @@
 
 variable "location" {
   type    = string
-  default = "eastus"
+  default = "australiaeast"
 }
 
 variable "naming_prefix" {
   type    = string
-  default = "adolabs"
+  default = "mylab"
 }
 
 variable "asp_tier" {
-    type = string
-    description = "Tier for App Service Plan (Standard, PremiumV2)"
-    default = "Free"
+  type        = string
+  description = "Tier for App Service Plan (Standard, PremiumV2)"
+  default     = "Free"
 }
 
 variable "asp_size" {
-    type = string
-    description = "Size for App Service Plan (S2, P1v2)"
-    default = "S1"
+  type        = string
+  description = "Size for App Service Plan (S2, P1v2)"
+  default     = "F1"
 }
 
 variable "capacity" {
-  type = string
+  type        = string
   description = "Number of instances for App Service Plan"
-  default = "1"
+  default     = "1"
 }
