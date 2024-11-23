1843cc88eeea180d1fc5779110d3ed82a1c790cf
martin.yang <abc108cn@hotmail.com>
2024-11-23 15:08:46 +1100

    Fix subnet address_prefix format in main.tf: change from list to single string

--- main/main.tf
+++ main/main.tf
@@ -23,12 +23,12 @@ resource "azurerm_virtual_network" "example" {
 
   subnet {
     name           = "subnet1"
-    address_prefix = ["10.0.1.0/24"]
+    address_prefix = "10.0.1.0/24"
   }
 
   subnet {
     name           = "subnet2"
-    address_prefix = ["10.0.2.0/24"]
+    address_prefix = "10.0.2.0/24"
     #security_group   = azurerm_network_security_group.example.id
   }
 
