8ea058626b5d5664666e0c2570365db2d066906b
martin.yang <abc108cn@hotmail.com>
2024-11-23 15:12:05 +1100

    Update subnet configurations in main.tf: rename existing subnets and add new subnets with specified address prefixes

--- main/main.tf
+++ main/main.tf
@@ -22,13 +22,37 @@ resource "azurerm_virtual_network" "example" {
   #dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
   subnet {
-    name           = "subnet1"
+    name           = "fw-subnet"
     address_prefix = "10.0.1.0/24"
   }
 
   subnet {
-    name           = "subnet2"
+    name           = "gw-subnet"
     address_prefix = "10.0.2.0/24"
+    #security_group   = azurerm_network_security_group.example.id
+  }
+
+  subnet {
+    name           = "vm-subnet"
+    address_prefix = "10.0.3.0/24"
+    #security_group   = azurerm_network_security_group.example.id
+  }
+
+  subnet {
+    name           = "pe-subnet"
+    address_prefix = "10.0.4.0/24"
+    #security_group   = azurerm_network_security_group.example.id
+  }
+
+  subnet {
+    name           = "app-subnet"
+    address_prefix = "10.0.5.0/24"
+    #security_group   = azurerm_network_security_group.example.id
+  }
+
+  subnet {
+    name           = "db-subnet"
+    address_prefix = "10.0.6.0/24"
     #security_group   = azurerm_network_security_group.example.id
   }
 
