6c8f1ca3b0820a52046872be411c3d3090265bf8
martin.yang <abc108cn@hotmail.com>
2024-11-23 14:23:23 +1100

    Refactor main.tf: remove app service resources and update virtual desktop resource locations

--- main/main.tf
+++ main/main.tf
@@ -2,11 +2,8 @@
 # LOCALS
 ##################################################################################
 
-
 locals {
-  resource_group_name   = "${var.naming_prefix}-${random_integer.name_suffix.result}"
-  app_service_plan_name = "${var.naming_prefix}-${random_integer.name_suffix.result}"
-  app_service_name      = "${var.naming_prefix}-${random_integer.name_suffix.result}"
+  resource_group_name = "${var.naming_prefix}-${random_integer.name_suffix.result}"
 }
 
 resource "random_integer" "name_suffix" {
@@ -15,64 +12,18 @@ resource "random_integer" "name_suffix" {
 }
 
 ##################################################################################
-# APP SERVICE
+# VIRTUAL DESKTOP
 ##################################################################################
-
-resource "azurerm_resource_group" "app_service" {
-  name     = local.resource_group_name
-  location = var.location
-
-  tags = {
-    owner       = "martinyang"
-    cost_center = "108"
-  }
-}
-
-resource "azurerm_app_service_plan" "app_service" {
-  name                = local.app_service_plan_name
-  location            = azurerm_resource_group.app_service.location
-  resource_group_name = azurerm_resource_group.app_service.name
-
-  sku {
-    tier     = var.asp_tier
-    size     = var.asp_size
-    capacity = var.capacity
-  }
-
-  tags = {
-    owner       = "martinyang"
-    cost_center = "108"
-  }
-}
-
-resource "azurerm_app_service" "app_service" {
-  name                = local.app_service_name
-  location            = azurerm_resource_group.app_service.location
-  resource_group_name = azurerm_resource_group.app_service.name
-  app_service_plan_id = azurerm_app_service_plan.app_service.id
-
-  source_control {
-    repo_url           = "https://github.com/ned1313/nodejs-docs-hello-world"
-    branch             = "main"
-    manual_integration = true
-    use_mercurial      = false
-  }
-
-  tags = {
-    owner       = "martinyang"
-    cost_center = "108"
-  }
-}
 
 resource "azurerm_virtual_desktop_workspace" "workspace" {
   name                = "ws-01"
-  location            = azurerm_resource_group.app_service.location
+  location            = var.location
   resource_group_name = "avd-rg"
 }
 
 resource "azurerm_virtual_desktop_host_pool" "hostpool" {
   name                             = "pd-hp-01"
-  location                         = azurerm_resource_group.app_service.location
+  location                         = var.location
   resource_group_name              = "avd-rg"
   type                             = "Personal"
   load_balancer_type               = "Persistent"
@@ -87,7 +38,7 @@ resource "azurerm_virtual_desktop_host_pool" "hostpool" {
 
 resource "azurerm_virtual_desktop_application_group" "desktopapp" {
   name                = "desktop-app-group"
-  location            = azurerm_resource_group.app_service.location
+  location            = var.location
   resource_group_name = "avd-rg"
   type                = "Desktop"
   host_pool_id        = azurerm_virtual_desktop_host_pool.hostpool.id
